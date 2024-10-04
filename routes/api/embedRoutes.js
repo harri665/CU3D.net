e4194741fec114b3bab3325e30abbb55d427d88e
Harrison <harrison.d.martin@gmail.com>
2024-10-04 10:52:01 -0600

    added contact route

--- routes/api/embedRoutes.js
+++ routes/api/embedRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { sendOrEditDiscordMessage  } = require('../../utils/Discord/discordBot'); // Import your sendDiscordMessage function
+const { sendOrEditDiscordMessage } = require('../../utils/Discord/discordBot'); // Import your sendDiscordMessage function
 
 router.post('/send-or-edit-message', async (req, res) => {
   const { messageId, channelId, content, embeds, components } = req.body;
@@ -13,6 +13,31 @@ router.post('/send-or-edit-message', async (req, res) => {
       content,
       embeds,
       components
+    );
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({ success: false, error: error.message });
+  }
+});
+
+// API Route to handle contact form submission
+router.post('/contact', async (req, res) => {
+  const { name, email, subject, message } = req.body;
+  const discordMessage = `
+  **New Contact Form Submission:**
+  - **Name:** ${name}
+  - **Email:** ${email}
+  - **Subject:** ${subject}
+  - **Message:** ${message}
+`;
+  try {
+    // Use the sendOrEditDiscordMessage function to either send or edit a message
+    const result = await sendOrEditDiscordMessage(
+      '509513851994374149', // Use the provided channelId or default one
+      null,
+      discordMessage,
+      null,
+      null
     );
     res.status(200).json(result);
   } catch (error) {
